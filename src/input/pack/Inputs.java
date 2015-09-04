04f194bbbce095029a7d275c0109721b5f73c9f6
madeast <emad6932@740s-pl-113.local>
2015-09-04 09:43:39 -0600

    Add it.

---
+++ src/input/pack/Inputs.java
@@ -0,0 +1,12 @@
+package input.pack;
+
+public class Inputs
+{
+	// Runners always look like this.
+	public static void main(String[] args)
+	{
+		InputController myInputController = new InputController();
+		myInputController.start();
+	}
+
+}
